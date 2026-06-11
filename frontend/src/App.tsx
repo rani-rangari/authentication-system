8434c074242238e0ee9693e04a58bf37fbc047dc
Rani <ranirangari2018@gmail.com>
2026-06-11 19:02:53 +0530

    refactor: update routing to include protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,29 @@ import MainLayout from "./components/layout/MainLayout";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
+import Dashboard from "./pages/Dashboard"; 
+import ProtectedRoute from "./routes/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext"; 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Pages WITH Navbar */}
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
-          {/* Other pages like About, Contact, etc */}
-        </Route>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Pages WITH Navbar */}
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Route>
 
-        {/* Auth Pages WITHOUT Navbar */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </BrowserRouter>
+          {/* Auth Pages WITHOUT Navbar */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace={true} />;
+};
+
+export default ProtectedRoute;
